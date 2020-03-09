cb3e74125c197079ea012d4568e3393e5a1f6df9
VadimFarutin <vfarut@gmail.com>
2020-03-10 01:17:52 +0300

    hw03 initial commit

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -148,24 +148,93 @@ def _get_pose_mat(point_all, corners, intrinsic_mat):
     return mat
 
 
+def _camera_pos_initialization(corner_storage, intrinsic_mat, parameters):
+    best_point_cloud_size = -1
+    best_pose = None
+    best_idx = 1
+
+    for i in range(1, len(corner_storage)):
+        print(f"Frame index: {i}/{len(corner_storage)}")
+        pose, pose_cloud_size = _check_pair(corner_storage[0],
+                                            corner_storage[i],
+                                            intrinsic_mat, parameters)
+        print(f"    Pose cloud size: {pose_cloud_size}")
+
+        if best_point_cloud_size < pose_cloud_size:
+            best_point_cloud_size = pose_cloud_size
+            best_pose = pose
+            best_idx = i
+
+    known_view_1 = (0, view_mat3x4_to_pose(eye3x4()))
+    known_view_2 = (best_idx, best_pose)
+
+    return known_view_1, known_view_2
+
+
+def _check_pair(corners1, corners2, intrinsic_mat, parameters):
+    correspondences = build_correspondences(corners1, corners2)
+
+    if len(correspondences.points_1) < 5 or len(correspondences.points_2) < 5:
+        return None, 0
+
+    essential_mat, mask_essential = cv2.findEssentialMat(points1=correspondences.points_1,
+                                                         points2=correspondences.points_2,
+                                                         cameraMatrix=intrinsic_mat)
+    inliers_cnt = np.sum(mask_essential != 0)
+    print(f"    Inliers for essential matrix: {inliers_cnt}")
+
+    _, mask_homography = cv2.findHomography(srcPoints=correspondences.points_1,
+                                            dstPoints=correspondences.points_2,
+                                            method=cv2.RANSAC,
+                                            ransacReprojThreshold=parameters.max_reprojection_error)
+    inliers_homography_cnt = np.sum(mask_homography != 0)
+    print(f"    Inliers for homography: {inliers_homography_cnt}")
+
+    if inliers_cnt < inliers_homography_cnt:
+        return None, 0
+
+    ids = np.argwhere(mask_essential == 0).astype(int)
+    correspondences = remove_correspondences_with_ids(correspondences, ids)
+
+    view_mat1 = eye3x4()
+    R1, R2, t = cv2.decomposeEssentialMat(E=essential_mat)
+    poses = [Pose(R1, t), Pose(R1, -t), Pose(R2, t), Pose(R2, -t)]
+    best_point_cloud_size = -1
+    best_pose = None
+
+    for pose in poses:
+        view_mat2 = pose_to_view_mat3x4(pose)
+        points, _, _ = triangulate_correspondences(correspondences,
+                                                   view_mat1, view_mat2,
+                                                   intrinsic_mat, parameters)
+        if best_point_cloud_size < len(points):
+            best_point_cloud_size = len(points)
+            best_pose = pose
+
+    return best_pose, best_point_cloud_size
+
+
 def track_and_calc_colors(camera_parameters: CameraParameters,
                           corner_storage: CornerStorage,
                           frame_sequence_path: str,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
 
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
         camera_parameters,
         rgb_sequence[0].shape[0]
     )
-
-    parameters = TriangulationParameters(max_reprojection_error=1.0,
-                                         min_triangulation_angle_deg=2.0,
+    parameters = TriangulationParameters(max_reprojection_error=3.0,
+                                         min_triangulation_angle_deg=3.0,
                                          min_depth=0.1)
+
+    # if known_view_1 is None or known_view_2 is None:
+    known_view_1, known_view_2 = _camera_pos_initialization(corner_storage,
+                                                            intrinsic_mat,
+                                                            parameters)
+    print(f"Found frames: {known_view_1[0]} and {known_view_2[0]}")
     view_mats, point_cloud_builder = _track_camera_int(corner_storage,
                                                        intrinsic_mat,
                                                        parameters,
